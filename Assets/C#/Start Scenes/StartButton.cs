51c66d5073866bd3d835fc761de252773839f951
tomato75 <zfj1412284981@email.com>
2026-06-19 10:43:48 +0800

    更新了开始游戏和敌人角色的攻击动画

--- Assets/C#/Start Scenes/StartButton.cs
+++ Assets/C#/Start Scenes/StartButton.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
-    
+    public GameObject Settings;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
